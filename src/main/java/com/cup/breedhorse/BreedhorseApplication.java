709b8ca48a5789c96a032e34e01953309e73bb1f
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-09-07 09:36:17 -0300

    first commit

---
+++ src/main/java/com/cup/breedhorse/BreedhorseApplication.java
@@ -0,0 +1,13 @@
+package com.cup.breedhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedhorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedhorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cup/breedhorse/BreedhorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cup.breedhorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreedhorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
