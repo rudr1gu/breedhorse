f14ddf2fd81a9536b8eec292186fbc3dd9c00978
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-09-07 11:39:50 -0300

    feat: Add Characteristic entity with JPA annotations and relationships

---
+++ src/main/java/com/cup/breedhorse/model/Characteristic.java
@@ -0,0 +1,34 @@
+package com.cup.breedhorse.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "characteristics")
+public class Characteristic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToMany(mappedBy = "characteristics")
+    private Set<BreedHorse> breed = new HashSet<>();
+}
